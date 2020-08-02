96f837ea0d57094b5a2013f53ce66d1a6817273d
Angelina Lameshkin <angelinalameshkin@Angelinas-MacBook-Air.local>
2020-08-02 19:03:44 +0300

    master

--- MAIN.h
+++ MAIN.h
@@ -6,6 +6,7 @@
 
 #define IC=100;
 #define DC=0;
+#define row_len=80;
 
 int error; /*global variable to mark an error*/
 
--- error_check.c
+++ error_check.c
@@ -4,64 +4,70 @@ void show_error(int num_er){
     switch(num_er){
 
         case 1:
-            printf("");
+            printf("there's no such function");
             break;
         case 2:
-            printf("");
+            printf("too many operands");
             break;
         case 3:
-            printf("");
+            printf("too little operands");
             break;
         case 4:
-            printf("");
+            printf("incorrect type of source operand ");
             break;
         case 5:
-            printf("");
+            printf("incorrect type of destination operand");
             break;
         case 6:
-            printf("");
+            printf("destination operand doesn't exist");
             break;
         case 7:
-            printf("");
+            printf("source operand doesn't exist");
             break;
         case 8:
-            printf("");
+            printf("incorrect structure of the statement");
             break;
         case 9:
-            printf("");
+            printf("label already exists");
             break;
         case 10:
-            printf("");
+            printf("labale's name is the same as the name of a saved word");
             break;
         case 11:
-            printf("");
+            printf("there's a white space after the colon");
             break;
         case 12:
-            printf("");
+            printf("missing quotation from a .string diractive statement");
             break;
         case 13:
-            printf("");
+            printf("usage of an undifined label");
             break;
         case 14:
-            printf("");
+            printf("label can't be a directive statement");
             break;
         case 15:
-            printf("");
+            printf("label can't be an instruction");
             break;
         case 16:
-            printf("");
+            printf("label is already defined as being external");
             break;
-        case 17:
-            printf("");
-            break;
+        /*case 17:
+            printf("label is already defined as being internal");
+            break;*/
         case 18:
-            printf("");
+            printf("not a number");
             break;
         case 19:
-            printf("");
+            printf("not a string");
             break;
         case 20:
-            printf("");
+            printf("token's length is higher than %d",row_len);
+            break;
+        case 21:
+            printf("row too long(more than %d characters)",row_len);
+            break;
+        case 22:
+            printf("invalid adress");
             break;
 
 
