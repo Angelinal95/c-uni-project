159e89c0039fe842dd61de8490c3173fc868f897
Angelina Lameshkin <angelinalameshkin@Angelinas-MacBook-Air.local>
2020-08-08 19:51:22 +0300

    master

--- first_pass.c
+++ first_pass.c
@@ -20,9 +20,9 @@ int main_pass(char *filename)
     char temp[81];
     const char s[2] = " ";
     char *token;
+    int error;
 
     fd = fopen(filename, "r");
-    return (0);
 
     if (fd == NULL)
     {
@@ -30,23 +30,30 @@ int main_pass(char *filename)
         return 1;
     }
 
-    line_count++;
-    temp = strcpy(temp, str);
-    temp = strtok(str, ' ');
-    token = strtok(temp, s);
-
     while (fgets(str, max_row_len, fd) != NULL)
 
     {
+        line_count++;
+        error = 0;
+
+        temp = strcpy(temp, str);
+        token = strtok(temp, s);
+
         //checking if the length of the line is more than 80 characters
         if (temp[81] != '/n')
         {
             show_error(20, line_count);
+            error++;
         }
+
         if (ignore_line)()==0)
             {
-                /* code */
+                go_through_line(token);
             }
+    }
+
+    int go_through_line(char *token)
+    {
 
         //checking if there's a label and taking care of it
         if (strchr(str, ':') != NULL)
@@ -55,59 +62,74 @@ int main_pass(char *filename)
         }
 
         //
-        token = strtok(NULL, s) if (token != NULL) {}
+        token = strtok(NULL, s)
 
-        if (check_if_com_or_dir() == 1)
+            if (check_if_com_or_dir() == 1)
         {
             /* code */
         }
+
+        token = strtok(NULL, s)
+
+            if (token != NULL)
+        {
+            token = strtok(NULL, s)
+
+                if (token != NULL)
+            {
+            }
+        }
     }
-}
-
-//checking if it's an empty line or a comment line
-int ignore_line()
-{
-
-    return 0;
-}
-
-//determine if it's a command line or a direction line
-int check_if_com_or_dir()
-{
-    if ()
+    //checking if it's an empty line or a comment line
+    int ignore_line()
     {
 
-        return 1; //for a direction
+        return 0;
     }
-    return 0; //for a command
-};
 
-int is_label()
-{
+    //determine if it's a command line or a direction line
+    int check_if_com_or_dir()
+    {
+        if ()
+        {
+
+            return 1; //for a direction
+        }
+        return 0; //for a command
+    };
+
+    int is_label()
+    {
+
+        return 0;
+    }
+
+    int is_string_or_data()
+    {
+
+        return 0;
+    }
+
+    int is_entry_or_extern()
+    {
+
+        return 0;
+    }
+    int insert_label_table()
+    {
+
+        return 0;
+    }
+
+    int skip_white_space()
+    {
+
+        return 0;
+    }
+
+    fclose(filename);
+
+    second_pass();
 
     return 0;
-}
-
-int is_string_or_data()
-{
-
-    return 0;
-}
-
-int is_entry_or_extern()
-{
-
-    return 0;
-}
-int insert_label_table()
-{
-
-    return 0;
-}
-
-fclose(filename);
-
-second_pass();
-
-return 0;
 }
