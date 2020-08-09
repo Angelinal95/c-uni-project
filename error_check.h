adf4da028a23f44d2d89c52ff46e3d6b2e844560
Nof <nofsh.sp@gmail.com>
2020-08-09 17:17:27 +0300

    new func

--- error_check.h
+++ error_check.h
@@ -2,4 +2,15 @@
 
 #define MAX_CHAR_ROW 80 /*Max number of chrecters in a row*/
 
-void show_error();
+void show_error(int,int);
+int legal_label(char*);
+int defined_label(char*); //When using a label in a command, we will check that such a label has been defined.
+int valid_number(char*);
+int valid_string(char*);
+int valid_directive(char*);
+int valid_variable(char*);
+int check_register(char*);
+int name_function(char*);
+int check_address(char*);
+int check_entry_label(char*);
+int check_extern_label(char*);
