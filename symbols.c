603b5f6ce454f3dac2e2be89ef0496b48b7714a6
Angelinal95 <69108503+Angelinal95@users.noreply.github.com>
2020-08-10 15:41:24 +0300

    Update symbols.c

--- symbols.c
+++ symbols.c
@@ -10,12 +10,21 @@ int insert_into_symbol_table(char *symbol_name, int num, char *type_of_symbol, s
     new_label->value = num;
     new_label->type_of_symbol = type_of_symbol;
     new_label->next = symbols_list;
+    symbols_list=new_label;
 
     return 0;
 }
 
-   insert_into_instruction_list(char *type_of_inst, char *info, char *pointer_to_row_in_symbol_table){
-   
+   insert_into_instruction_list(char *type_of_inst, char *info, char *pointer_to_row_in_symbol_table, instruction_line *instruction_line_list){
+       instruction_line *new_inst_line;
+       new_inst_line=(instruction_line*)malloc(sizeof(instruction_line));
+       new_inst_line->label=pointer_to_row_in_symbol_table;
+       new_inst_line->type_of_inst=type_of_inst;
+       new_inst_line->info=info;
+       new_inst_line->next=instruction_line_list;
+       instruction_line_list=new_inst_line;
+       
+      
         return 0;
    }
 
