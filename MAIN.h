c42c687cb83397450d181011064deb22251fd1cb
Angelinal95 <69108503+Angelinal95@users.noreply.github.com>
2020-08-10 14:42:24 +0300

    Update MAIN.h

--- MAIN.h
+++ MAIN.h
@@ -77,11 +77,11 @@ int error;    //global variable to mark an error
 /*------------------------Register List ------------------------*/
 
 char registers[] = {
-    {"r_0"}, {"r_1"}, {"r_2"}, {"r_3"}, {"r_4"}, {"r_5"}, {"r_6"}, {"r_7"}};
+    {"r_0"}, {"r_1"}, {"r_2"}, {"r_3"}, {"r_4"}, {"r_5"}, {"r_6"}, {"r_7"},{ NULL }};
 
 
 /*------------------------Commands List ------------------------*/
 
 
-command command_list[] = {{"mov", NULL, 0,2}, {"cmp",NULL,1,2}, {"add",1,2,2}, {"sub",2,2,2}, {"lea",NULL,4,2}, {"clr",1,5,1}, {"not",2,5,1}, {"inc",3,5,1}, {"dec",4,5,1}, {"jmp",1,9,1}, {"bne",2,9,1}, {"jsr"3,9,1}, {"red",NULL,12,1}, {"prn",NULL,13,1}, {"rts",NULL,14,0}, {"stop",NULL,15,0}};
+command command_list[] = {{"mov", NULL, 0,2}, {"cmp",NULL,1,2}, {"add",1,2,2}, {"sub",2,2,2}, {"lea",NULL,4,2}, {"clr",1,5,1}, {"not",2,5,1}, {"inc",3,5,1}, {"dec",4,5,1}, {"jmp",1,9,1}, {"bne",2,9,1}, {"jsr"3,9,1}, {"red",NULL,12,1}, {"prn",NULL,13,1}, {"rts",NULL,14,0}, {"stop",NULL,15,0},{ NULL }};
 
