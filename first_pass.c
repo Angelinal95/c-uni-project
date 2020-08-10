579ad7d52442db056b302ec866e85f9fa03935c3
Angelinal95 <69108503+Angelinal95@users.noreply.github.com>
2020-08-10 14:24:10 +0300

    Update first_pass.c

--- first_pass.c
+++ first_pass.c
@@ -1,10 +1,7 @@
 #include "MAIN.h"
 #include "error_check.h"
 
-index_term registers[] = {
-    {"r_0"}, {"r_1"}, {"r_2"}, {"r_3"}, {"r_4"}, {"r_5"}, {"r_6"}, {"r_7"}};
 
-index_term command_list[] = {{"mov"}, {"cmp"}, {"add"}, {"sub"}, {"lea"}, {"clr"}, {"not"}, {"inc"}, {"dec"}, {"jmp"}, {"bne"}, {"jsr"}, {"red"}, {"prn"}, {"rts"}, {"stop"}};
 
 int main_pass(char *filename)
 {
