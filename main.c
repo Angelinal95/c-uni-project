03f2b6a5d96e93751c25c1e220d7c999a2b13d25
Nof Shabtay <nofsh.sp@gmail.com>
2020-08-21 01:46:26 +0300

    up one line

--- main.c
+++ main.c
@@ -4,26 +4,32 @@ int main(int argc, char *argv[])
 {
     int i, numOfErrors = 0;
 
-    for (i = 1; i < argc; i++)
+    if (argc >= 1)
     {
-        /* First Read */
-        numOfErrors += first_pass(argv[i]);
-        /* Second Read */
-        numOfErrors += secondPass(argv[i]);
+        for (i = 1; i < argc; i++)
+        {
+            /* First Read */
+            numOfErrors += first_pass(argv[i]);
+            /* Second Read */
+            numOfErrors += secondPass(argv[i]);
 
-        /* If there are no errors */
-	    if (numOfErrors == 0)
-	    {
-		    /* Create all the output files */
-		    ObjectFile(argv[i], IC, DC/*, memoryArr*/);
-		    ExternFile(argv[i]/*, linesArr, numOflines*/);
-		    EntriesFile(argv[i]);
-		    printf("success, output files for the file \"%s.as\" were created.\n", argv[i]);
-	    }
-	    return 1;
+            /* If there are no errors */
+	        if (numOfErrors == 0)
+	        {
+		        /* Create all the output files */
+		        ObjectFile(argv[i], IC, DC/*, memoryArr*/);
+		        ExternFile(argv[i]/*, linesArr, numOflines*/);
+		        EntriesFile(argv[i]);
+		        printf("success, output files for the file \"%s.as\" were created.\n", argv[i]);
+	        }
+	    
+        }
+        return 1;
     }
 
-    return 0;
+    else
+        return 0;
+        
 }
 	
 
