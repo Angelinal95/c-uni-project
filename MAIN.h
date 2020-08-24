536fad7c50265c52e5343dfb0e82e9c4a4963332
Nof Shabtay <nofsh.sp@gmail.com>
2020-08-24 23:52:13 +0300

    .

--- MAIN.h
+++ MAIN.h
@@ -112,10 +112,6 @@ void erase_instruction_list(struct instruction_line *);                         
 void erase_instruction_line(struct instruction_line *);                                            //erasing the current node from the instruction line
 void erase_command_line(struct command_line *);                                                    //erasing the current node from the command line
 
-/*------------------------Register List ------------------------*/
-
-char registers[] = {
-    {"r_0"}, {"r_1"}, {"r_2"}, {"r_3"}, {"r_4"}, {"r_5"}, {"r_6"}, {"r_7"}, {NULL}};
 
 /*------------------------Commands List ------------------------*/
 command command_list[] = {{"mov", 0, 0, 2}, {"cmp", 0, 1, 2}, {"add", 1, 2, 2}, {"sub", 2, 2, 2}, {"lea", 0, 4, 2}, {"clr", 1, 5, 1}, {"not", 2, 5, 1}, {"inc", 3, 5, 1}, {"dec", 4, 5, 1}, {"jmp", 1, 9, 1}, {"bne", 2, 9, 1}, {"jsr", 3, 9, 1}, {"red", 0, 12, 1}, {"prn", 0, 13, 1}, {"rts", 0, 14, 0}, {"stop", 0, 15, 0}, {NULL}};
